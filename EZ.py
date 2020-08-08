eec7f3b8ee82893b21375469a2c9053a864f955e
QD888 <69104074+QD888@users.noreply.github.com>
2020-08-08 19:13:44 +0800

    Update EZ.py

--- EZ.py
+++ EZ.py
@@ -7,6 +7,7 @@
 # Tested: 8th Aug. 2020
 # Get REST api Documents
 # https://IP address:8443/univmax/restapi/docs
+# notes: "91" in url is unisphere version
 
 # Import libraries
 import requests
@@ -18,10 +19,10 @@ from urllib.parse import quote
 
 
 # Set up endpoint and authentication credentials
-endpoint = 'https://IP address:8443/univmax/restapi/91'
+endpoint = 'https://IP address:8443/univmax/restapi'
 
 # Get base system information--symmetrixId
-api = '/system/symmetrix'
+api = '/91/system/symmetrix'
 url = endpoint + api
 username = 'username'
 password = 'password'
@@ -37,9 +38,12 @@ print(res.text)
 
 payload = json.loads(res.text)
 symmID = payload['symmetrixId'][0]
+# if multi VMAX/PowerMax, get symmetrixId, to be used in performance query
+symmID2 = payload['symmetrixId'][2]
 
 
 # Get response, example: system basic information, and show disk number
+api = '/91/system/symmetrix'
 url  = endpoint +api + "/" + symmID
 res1 = requests.get(url, headers=headers, auth=auth, verify=False)
 print(res1.text)
@@ -48,8 +52,29 @@ diskNum = payload1['disk_count']
 print(diskNum)
 
 # Get response, example: system capacity information
-url  = endpoint + "/sloprovisioning/symmetrix/" + symmID
+url  = endpoint + "/91/sloprovisioning/symmetrix/" + symmID
 res2 = requests.get(url, headers=headers, auth=auth, verify=False)
 payload2 = json.loads(res2.text)
 CapTotal = payload2['system_capacity']['subscribed_total_tb']
 print('System Capacity: ', CapTotal, 'TB')
+
+
+# Post Response, example: system performance
+api = '/performance/Array/metrics'
+url  = endpoint + api
+data = {
+  "symmetrixId": symmID2,
+  "startDate": 1596880800000,
+  "endDate": 1596880920000,
+  "metrics": [
+      "PercentHit","PercentCacheWP","PercentReads","PercentWrites","FEUtilization","ReadResponseTime","WriteResponseTime"
+  ]
+}
+headers = {
+    'Content-Type': "application/json",
+    'Accept': "application/json",
+    'Accept-Language': 'en-US'
+}
+res3 = requests.post(url, auth=auth, headers=headers, json=data, verify=False)
+payload3 = json.loads(res3.text)
+print(payload3)

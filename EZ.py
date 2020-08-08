64b0abd7f832b1334edc218ce0cdcffeec32288d
QD888 <69104074+QD888@users.noreply.github.com>
2020-08-08 12:32:32 +0800

    Create EZ.py

---
+++ EZ.py
@@ -0,0 +1,10 @@
+# OpenOPS - PowerMax, Dell EMC PowerMax/VMAX Monitor
+# PowerMax: PowerMax 2000
+# Unisphere Version: 9.1.0.9
+# Programe: Python 3.8
+# Stage: prototype
+# Author: QD888
+# Tested: 8th Aug. 2020
+
+# Get REST api Documents
+# https://IP address:8443/univmax/restapi/docs
